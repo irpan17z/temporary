de7f68b2831ada7bf25a4d0e241d5e784dd2247a
irpan17z <bahtiarirfan813@gmail.com>
2022-06-18 19:43:42 +0700

    build evolution-x

--- build_rom.sh
+++ build_rom.sh
@@ -1,15 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/ProjectBlaze/manifest -b 12.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/putraaxyzo/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b snow -g 
+git clone https://github.com/irpan17z/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 . build/envsetup.sh
-export WITH_GAPPS=true
 export SELINUX_IGNORE_NEVERALLOWS=true
-lunch blaze_juice-userdebug
+lunch evolution_mido-userdebug
 export TZ=Asia/Jakarta #put before last build command
-brunch juice
+mka evolution
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
